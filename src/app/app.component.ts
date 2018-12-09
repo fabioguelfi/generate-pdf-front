a77a29dbc19e4c3e3e99cbc7165feb0af97a84c5
fabioguelfi <fabioguelfiunix@gmail.com>
2018-12-09 14:56:38 -0200

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import * as jsPDF from 'jspdf'
+import * as html2canvas from 'html2canvas'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rxjs-exemple';
+  protected idViews: Array<string> = ['teste', 'teste2', 'teste3']
+  protected images: Array<string> = []
+
+  public generateCanvas() {
+    return new Promise((resolve, reject) => {
+      this.idViews.map((id, i, arr) => {
+        const lastIndex = this.idViews.length - 1
+        if (i === lastIndex) {
+          html2canvas(document.querySelector(`#${id}`)).then(canvas => {
+            this.images.push(canvas)
+            resolve()
+          })
+        } else {
+          html2canvas(document.querySelector(`#${id}`)).then(canvas => {
+            this.images.push(canvas)
+          })
+        }
+      })
+    })
+  }
+
+  public callPDF() {
+    this.generateCanvas()
+      .then(() => {
+        const doc = new jsPDF()
+        this.images.map((image, i) => {
+          const lastIndex = this.images.length - 1
+          doc.addImage(image, 'JPEG', 15, 40, 180, 180)
+          if (i === lastIndex) {
+            doc.save()
+          } else {
+            doc.addPage();
+          }
+        })
+      })
+  }
 }
